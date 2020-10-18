b5a501fc338c87beedd7209347c6fdb1967ff35e
AndyGeek <andy.eulogio@gmail.com>
2020-10-17 20:58:54 -0500

    Finish menu buttons in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./styles/style.scss";
 import { Navbar } from "./components/Navbar";
+import { HomePage } from "./pages/HomePage";
+import { ShopPage } from "./pages/ShopPage";
 
 export const App = () => {
   return (
-    <div className="container">
-      <Navbar />
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/shop">
+            <ShopPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <div className="navbar__container">
       <div className="navbar__logo">
-        <img src="./logo.png" alt=""/>
+        <img src="./logo.png" alt="" />
       </div>
       <div className="navbar__menu">
-        Menú
+        <Link>HOME</Link>
+        <Link>SHOP</Link>
+        <Link>PAGES</Link>
+        <Link>BLOG</Link>
+        <Link>CONTACT</Link>
       </div>
-      <div className="navbar__shopping">
-        Compras
-      </div>
+      <div className="navbar__shopping">Compras</div>
     </div>
   );
 };
