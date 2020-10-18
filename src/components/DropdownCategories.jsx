4c8b82b0997788298dbc4c847720bfc49156531e
AndyGeek <andy.eulogio@gmail.com>
2020-10-18 00:49:21 -0500

    Add button all departament

--- src/components/DropdownCategories.jsx
+++ src/components/DropdownCategories.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { ReactComponent as MenuIcon } from "../icons/menu.svg";
+import { ReactComponent as ArrowIcon } from "../icons/arrow.svg";
 
 export const DropdownCategories = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="drowdowncategories__container">
-      <h1>All department</h1>
+    <div className="ddcategories__container">
+      <div
+        className="ddcategories__title"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        <span className="ddcategories__menu-icon">
+          <MenuIcon />
+        </span>
+        <h3 className="ddcategories__title">All department</h3>
+        <span className="ddcategories__arrow-icon">
+          <ArrowIcon />
+        </span>
+      </div>
+      {open ? (
+        <div className="ddcategories__toggler">
+          <ul className="ddcategories__list">
+            <li className="ddcategories__item">Fresh Meat</li>
+            <li className="ddcategories__item">Fruit & Nut Gifts</li>
+            <li className="ddcategories__item">Vegetables</li>
+            <li className="ddcategories__item">Ocean Foods</li>
+            <li className="ddcategories__item">Fastfood</li>
+            <li className="ddcategories__item">Fresh Bananas</li>
+          </ul>
+        </div>
+      ) : null}
     </div>
   );
 };
